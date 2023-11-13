6cef241fb1cbaddd9209de4278412bea3e8965fa
anhm-ng <aminhng3012@gmail.com>
2023-11-13 09:48:21 +0700

    update vite multiple app

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
+        tutor: resolve(__dirname, 'tutor.html')
       },
     },
   },
