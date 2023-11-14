49092e57e0b3fc066a878011964fe85205af8f30
anhm-ng <aminhng3012@gmail.com>
2023-11-14 22:35:15 +0700

    Big Update
    - Before Kinder IELTS's feedbacks

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         tutor: resolve(__dirname, 'tutor.html'),
-        hatgiong: resolve(__dirname, 'hatgiong.html')
+        hatgiong: resolve(__dirname, 'hatgiong.html'),
+        mam: resolve(__dirname, 'mam.html'),
+        choi: resolve(__dirname, 'choi.html'),
+        la: resolve(__dirname, 'la.html'),
+        bonhatcore: resolve(__dirname, 'bon.html'),
       },
     },
   },
